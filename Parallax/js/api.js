b5a2f44ddccc305089dfeb6dd9ca1f066cd010bf
Luan Cruz <luanncruzz20@gmail.com>
2022-09-06 15:34:44 -0400

    att

--- Parallax/js/api.js
+++ Parallax/js/api.js
@@ -1,20 +1,37 @@
-async function getData(){
-    try{
-        const config = {
-            method: 'GET',
-            mode: 'cors',
-            cache: 'default'
+const listaP = document.querySelector("select#personagem");
 
-        };
+async function getData() {
+  try {
+    const config = {
+      method: "GET",
+      mode: "cors",
+      cache: "default",
+    };
 
-       await fetch("https://rickandmortyapi.com/api/character", config)
-            .then((data)=> {
-                console.log(data)
-            })
-    
-    }catch(error){
-        console.log(error)
-    }
-    
+    const res = await fetch(
+      "https://rickandmortyapi.com/api/character",
+      config
+    );
+    const data = await res.json();
+
+    const listaFiltrada = data.results.map((item) => {
+      return {
+        name: item.name,
+        image: item.image,
+      };
+    });
+
+    listaFiltrada.map((states) => {
+      const optt = document.createElement("option");
+      optt.text = states.name;
+      optt.value = states.image;
+      listaP.appendChild(optt);
+    });
+
+    console.log(data);
+    console.log(listaFiltrada);
+  } catch (error) {
+    console.log(error);
+  }
 }
-getData()
+getData();
